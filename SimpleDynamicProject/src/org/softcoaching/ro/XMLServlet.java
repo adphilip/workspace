5cd11b5a4b9abf604e0790e9c7f06f8746a4dae8
adrian filipescu <adrian.filipescu@gmail.com>
2015-12-29 17:32:30 +0200

    Post Method - forget tis function

--- SimpleDynamicProject/src/org/softcoaching/ro/XMLServlet.java
+++ SimpleDynamicProject/src/org/softcoaching/ro/XMLServlet.java
@@ -20,12 +20,31 @@ public class XMLServlet extends HttpServlet {
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		
 		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
 		
+		//request parameter
+		//http://localhost:8080/SimpleDynamicProject/XMLServletPath?userName=Adrian
+		String userName = request.getParameter("userName");
+		out.println("<h3>Ce faci in do Get method din servlet via XML?</h3> <br> <h4>Ia ce fac de mult </h4> " + userName);
 		
-		PrintWriter writer = response.getWriter();
-		writer.println("<h3>Ce faci in do Get method din servlet via XML?</h3> <br> <h4>Ia ce fac de mult </h4> ");
 		//e afisat in consola
-		System.out.println("Hi from doGet method in XML Servlet!");
+		System.out.println("Hi from doGet method in XML Servlet!, userName :" + userName);
+	}
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		//request parameter
+		//http://localhost:8080/SimpleDynamicProject/XMLServletPath?userName=Adrian
+		String userName = request.getParameter("userName");
+		out.println("<h3>Ce faci in do POST ?</h3>  " + userName);
+		
+		//e afisat in consola
+		System.out.println("Hi from doPost method in XML Servlet!, userName :" + userName);
 	}
 
 }
